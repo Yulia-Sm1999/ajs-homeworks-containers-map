565d6c04365913529f212eeec0bc761a82ddb05f
Юлия Смирнова <dolphin_99@mail.ru>
2023-01-01 20:30:17 +0300

    List of errors added in class constructor ErrorRepository.js

--- __test__/ErrorRepository.test.js
+++ __test__/ErrorRepository.test.js
@@ -2,18 +2,12 @@ import ErrorRepository from '../src/ErrorRepository';
 
 test('should show description of the error', () => {
   const errorsList = new ErrorRepository();
-  errorsList.errors.set(404, 'Not Found');
-  errorsList.errors.set(503, 'Service Unavailable');
-  errorsList.errors.set(403, 'Forbidden');
 
   expect(errorsList.translate(404)).toBe('Not Found');
 });
 
 test('should show "Unknown error"', () => {
   const errorsList = new ErrorRepository();
-  errorsList.errors.set(404, 'Not Found');
-  errorsList.errors.set(503, 'Service Unavailable');
-  errorsList.errors.set(403, 'Forbidden');
 
   expect(errorsList.translate(507)).toBe('Unknown error');
 });
--- src/ErrorRepository.js
+++ src/ErrorRepository.js
@@ -1,6 +1,9 @@
 export default class ErrorRepository {
   constructor() {
     this.errors = new Map();
+    this.errors.set(404, 'Not Found');
+    this.errors.set(503, 'Service Unavailable');
+    this.errors.set(403, 'Forbidden');
   }
 
   translate(code) {
